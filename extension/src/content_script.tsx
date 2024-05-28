55b50e365767d2bb9f027c3b1e9f520018c088b3
= <=>
2024-05-28 00:08:52 -0400

    Scaffolding for styling

--- extension/src/content_script.tsx
+++ extension/src/content_script.tsx
@@ -1,6 +1,5 @@
 console.log("Hello from content.ts");
 // Send message to service worker
 chrome.runtime.sendMessage({ greeting: "group" }, (response) => {
-  console.log(response.farewell);
-  alert("Grouping tabs...");
+  alert(response.farewell);
 });
