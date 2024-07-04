dd54154da3dcc480066ad46b8ebb08999ad87c7f
Taimur Shaikh <taimurshaikh2004@gmail.com>
2024-07-03 20:01:55 -0400

    Updated frontend with logos and color overhaul

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -42,18 +42,18 @@ const App: React.FC = () => {
 
   return (
     <div className="bg-gray-100 items-center p-4 w-[200px]">
-      <h1 className="text-3xl font-bold mb-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
-        TabZen <span>🧘🏽</span>
-      </h1>
+      <header className="flex flex-col items-start">
+        <img src="vector/default.svg" alt="Logo" className="-m-6" />
+      </header>
       <p className="text-gray-600 mb-4 text-base">
-        The world's <span className="font-bold text-slate-950">smartest</span>{" "}
-        tab manager. Group your workspace with just one click.
+        TabZen is the world's smartest tab manager. Group your workspace with
+        just <span className="font-bold text-slate-950">one click</span>.
       </p>
       <button
-        className="relative px-8 py-2 rounded overflow-hidden border border-purple-500 bg-white text-purple-500 shadow-2xl 
-        transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute 
-        after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-purple-500 
-        hover:before:w-2/4 hover:before:bg-gradient-to-r from-purple-500 to-purple-700 hover:after:w-2/4 hover:after:bg-purple-700"
+        className="relative px-8 py-2 rounded overflow-hidden border border-[#4E1A70] bg-white text-[#4E1A70] shadow-2xl
+    transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute
+    after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-[#4E1A70]
+    hover:before:w-2/4 hover:before:bg-gradient-to-r from-[#4E1A70] to-[#2B4CF2] hover:after:w-2/4 hover:after:bg-[#2B4CF2]"
         onClick={handleGroupTabs}
       >
         <span className="relative z-10 text-base">Group</span>
